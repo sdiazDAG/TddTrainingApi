59fca659e803d6f13dab7f7e2998a956a00ee62c
Santi Diaz <sdiaz@aidacanarias.com>
2021-12-07 11:30:07 +0000

    Add api EndPoint and Swuagger to calculate the number factorial

--- TddTrainingApi/Controllers/FactorialController.cs
+++ TddTrainingApi/Controllers/FactorialController.cs
@@ -1,9 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using Factorial;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TddTrainingApi.Controllers
 {
     public class FactorialController : Controller
     {
-        
+        [HttpPost("calculate/{number}")]
+        public long Calculate(int number)
+        {
+            try
+            {
+                long result = FactorialHandler.Calculate(number);
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
--- TddTrainingApi/Startup.cs
+++ TddTrainingApi/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
 namespace TddTrainingApi
 {
@@ -26,6 +27,7 @@ namespace TddTrainingApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            AddSwagger(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +40,12 @@ namespace TddTrainingApi
 
             app.UseHttpsRedirection();
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Foo API V1");
+            });
+
             app.UseRouting();
 
             app.UseAuthorization();
@@ -45,6 +53,27 @@ namespace TddTrainingApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+        }
+
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                var groupName = "v1";
+
+                options.SwaggerDoc(groupName, new OpenApiInfo
+                {
+                    Title = $"Foo {groupName}",
+                    Version = groupName,
+                    Description = "Foo API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Foo Company",
+                        Email = string.Empty,
+                        Url = new Uri("https://foo.com/"),
+                    }
+                });
             });
         }
     }
